b1409cb1ad4f2570b5c64b4a6e2126fad4cdd9f8
Pawlik123 <elzbieta1@opoczta.pl>
2020-03-27 11:44:06 +0100

    Fix tests

--- openx/OpenXTest.java
+++ openx/OpenXTest.java
@@ -19,34 +19,33 @@ public class OpenXTest {
 		JSONArray postslist = posts.getJSONArray();
 
 		OpenX a = new OpenX();
-		a.howManyPostsWriteUsers(userlist, postslist);
 		List<String> q = a.howManyPostsWriteUsers(userlist, postslist);
 		Assert.assertNotNull(q);
 	}
+
 	@Test
-	public void checkthePosthowManyPostsAreSimilarNotNull(){
+	public void checkthePosthowManyPostsAreSimilarNotNull() {
 		ParaseJSON users = new ParaseJSON("https://jsonplaceholder.typicode.com/users");
 		ParaseJSON posts = new ParaseJSON("https://jsonplaceholder.typicode.com/posts");
 
 		JSONArray userlist = users.getJSONArray();
 		JSONArray postslist = posts.getJSONArray();
-	
-		OpenX b=new OpenX();
-		b.howManyPostsAreSimilar(postslist);
-		List<String> qq=b.howManyPostsAreSimilar(postslist);
+
+		OpenX b = new OpenX();
+		List<String> qq = b.howManyPostsAreSimilar(postslist);
 		Assert.assertNotNull(qq);
-	}	
+	}
+
 	@Test
-	public void checkthePostwhatLocationUsers(){
+	public void checkthePostwhatLocationUsers() {
 		ParaseJSON users = new ParaseJSON("https://jsonplaceholder.typicode.com/users");
 		ParaseJSON posts = new ParaseJSON("https://jsonplaceholder.typicode.com/posts");
 
 		JSONArray userlist = users.getJSONArray();
 		JSONArray postslist = posts.getJSONArray();
-		
-		OpenX c=new OpenX();
-		c.whatLocationUsers(userlist);
-		List<String> qqq=c.whatLocationUsers(userlist);
+
+		OpenX c = new OpenX();
+		List<String> qqq = c.whatLocationUsers(userlist);
 		Assert.assertNotNull(qqq);
 	}
 }
